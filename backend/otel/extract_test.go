Brandon-duff/lighttrace

commit d38eb213c9e573820a1d839502ddf78e1fc4e334

fix writing a backend error stacktrace (#6121)
## Summary

<!--
Ideally, there is an attached GitHub issue that will describe the "why".

If relevant, use this section to call out any additional information
you'd like to _highlight_ to the reviewer.
-->

A regression was introduced in #6053 whereby backend errors were missing
their stacktraces. This was due to the fact that we weren't plucking the
data out ahead of time in the `extractFields` function.

## How did you test this change?

<!--
Frontend - Leave a screencast or a screenshot to visually describe the
changes.
-->

Verified stacktrace (and other error data) is populated:
![Screenshot 2023-07-26 at 12 53 15
PM](https://github.com/highlight/highlight/assets/58678/5677e04c-8b52-4a0f-bf43-aa8dd2fed1ac)

Verified we have a stacktrace with a new error in the UI:
![Screenshot 2023-07-26 at 12 55 26
PM](https://github.com/highlight/highlight/assets/58678/50e288f1-0bba-4e8a-a51c-dd7e45dc32e8)


## Are there any deployment considerations?

<!--
 Backend - Do we need to consider migrations or backfilling data?
-->

N/A

backend/otel/extract.go
@@ -29,6 +29,16 @@ type extractedFields struct {
 	source         modelInputs.LogSource
 	serviceName    string
 	serviceVersion string
+	logSeverity    string
+	logMessage     string
+
+	exceptionType       string
+	exceptionMessage    string
+	exceptionStackTrace string
+	errorUrl            string
+
+	metricEventName  string
+	metricEventValue string // up to the consumer to parse this into an expected format
 
 	// This represents the merged result of resource, span...log attributes
 	// _after_ we extract fields out. In other words, if `serviceName` is extracted, it won't be included
@@ -118,7 +128,47 @@ func extractFields(ctx context.Context, params extractFieldsParams) (extractedFi
 		delete(attrs, highlight.RequestIDAttribute)
 	}
 
-	if val, ok := resourceAttributes[string(semconv.ServiceNameKey)]; ok { // we know that service name will be in the resource hash
+	if val, ok := attrs[highlight.LogSeverityAttribute]; ok {
+		fields.logSeverity = val.(string)
+		delete(attrs, highlight.LogSeverityAttribute)
+	}
+
+	if val, ok := attrs[highlight.LogMessageAttribute]; ok {
+		fields.logMessage = val.(string)
+		delete(attrs, highlight.LogMessageAttribute)
+	}
+
+	if val, ok := attrs[highlight.MetricEventName]; ok {
+		fields.metricEventName = val.(string)
+		delete(attrs, highlight.MetricEventName)
+	}
+
+	if val, ok := attrs[highlight.MetricEventValue]; ok {
+		fields.metricEventValue = val.(string)
+		delete(attrs, highlight.MetricEventValue)
+	}
+
+	if val, ok := eventAttributes[string(semconv.ExceptionTypeKey)]; ok { // we know that exception.type will be in the event attributes map
+		fields.exceptionType = val.(string)
+		delete(attrs, string(semconv.ExceptionTypeKey))
+	}
+
+	if val, ok := eventAttributes[string(semconv.ExceptionMessageKey)]; ok { // we know that exception.message will be in the event attributes map
+		fields.exceptionMessage = val.(string)
+		delete(attrs, string(semconv.ExceptionMessageKey))
+	}
+
+	if val, ok := eventAttributes[string(semconv.ExceptionStacktraceKey)]; ok { // we know that exception.stacktrace will be in the event attributes map
+		fields.exceptionStackTrace = val.(string)
+		delete(attrs, string(semconv.ExceptionStacktraceKey))
+	}
+
+	if val, ok := eventAttributes[highlight.ErrorURLAttribute]; ok { // we know that URL will be in the event attributes map
+		fields.errorUrl = val.(string)
+		delete(attrs, highlight.ErrorURLAttribute)
+	}
+
+	if val, ok := resourceAttributes[string(semconv.ServiceNameKey)]; ok { // we know that service name will be in the resource map
 		fields.serviceName = val.(string)
 		delete(attrs, string(semconv.ServiceNameKey))
 	}
@@ -142,7 +192,7 @@ func extractFields(ctx context.Context, params extractFieldsParams) (extractedFi
 
 	attributesMap := make(map[string]string)
 	for k, v := range attrs {
-		prefixes := highlight.InternalAttributePrefixes
+		prefixes := []string{}
 		if fields.source == modelInputs.LogSourceFrontend {
 			prefixes = append(prefixes, highlight.BackendOnlyAttributePrefixes...)
 		}

backend/otel/otel.go
@@ -22,7 +22,6 @@ import (
 	"github.com/highlight-run/highlight/backend/public-graph/graph/model"
 	"github.com/highlight-run/highlight/backend/stacktraces"
 	"github.com/highlight/highlight/sdk/highlight-go"
-	hlog "github.com/highlight/highlight/sdk/highlight-go/log"
 	"github.com/openlyinc/pointy"
 	e "github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
@@ -52,18 +51,15 @@ func cast[T string | int64 | float64](v interface{}, fallback T) T {
 	return c
 }
 
-func getBackendError(ctx context.Context, ts time.Time, fields extractedFields, traceID, spanID string, logCursor *string, excMessage string) (bool, *model.BackendErrorObjectInput) {
-	excType := cast(fields.attrs[string(semconv.ExceptionTypeKey)], fields.source.String())
-	errorUrl := cast(fields.attrs[highlight.ErrorURLAttribute], fields.source.String())
-	stackTrace := cast(fields.attrs[string(semconv.ExceptionStacktraceKey)], "")
-	if excType == "" && excMessage == "" {
+func getBackendError(ctx context.Context, ts time.Time, fields extractedFields, traceID, spanID string, logCursor *string) (bool, *model.BackendErrorObjectInput) {
+	if fields.exceptionType == "" && fields.exceptionMessage == "" {
 		lg(ctx, fields).Error("otel received exception with no type and no message")
 		return false, nil
-	} else if stackTrace == "" || stackTrace == "null" {
+	} else if fields.exceptionStackTrace == "" || fields.exceptionStackTrace == "null" {
 		lg(ctx, fields).Warn("otel received exception with no stacktrace")
-		stackTrace = ""
+		fields.exceptionStackTrace = ""
 	}
-	stackTrace = stacktraces.FormatStructureStackTrace(ctx, stackTrace)
+	fields.exceptionStackTrace = stacktraces.FormatStructureStackTrace(ctx, fields.exceptionStackTrace)
 	payloadBytes, _ := json.Marshal(fields.attrs)
 	err := &model.BackendErrorObjectInput{
 		SessionSecureID: &fields.sessionID,
@@ -71,13 +67,13 @@ func getBackendError(ctx context.Context, ts time.Time, fields extractedFields, 
 		TraceID:         pointy.String(traceID),
 		SpanID:          pointy.String(spanID),
 		LogCursor:       logCursor,
-		Event:           excMessage,
-		Type:            excType,
+		Event:           fields.exceptionMessage,
+		Type:            fields.exceptionType,
 		Source:          fields.source.String(),
-		StackTrace:      stackTrace,
+		StackTrace:      fields.exceptionStackTrace,
 		Timestamp:       ts,
 		Payload:         pointy.String(string(payloadBytes)),
-		URL:             errorUrl,
+		URL:             fields.errorUrl,
 	}
 	if fields.sessionID != "" {
 		return false, err
@@ -89,11 +85,10 @@ func getBackendError(ctx context.Context, ts time.Time, fields extractedFields, 
 }
 
 func getMetric(ctx context.Context, ts time.Time, fields extractedFields, traceID, spanID string) (*model.MetricInput, error) {
-	name, ok := fields.attrs[highlight.MetricEventName]
-	if !ok {
+	if fields.metricEventName == "" {
 		return nil, e.New("otel received metric with no name")
 	}
-	value, err := strconv.ParseFloat(fields.attrs[highlight.MetricEventValue], 64)
+	value, err := strconv.ParseFloat(fields.metricEventValue, 64)
 
 	if err != nil {
 		return nil, e.New("otel received metric with no value")
@@ -101,7 +96,7 @@ func getMetric(ctx context.Context, ts time.Time, fields extractedFields, traceI
 	return &model.MetricInput{
 		SessionSecureID: fields.sessionID,
 		Group:           pointy.String(fields.requestID),
-		Name:            name,
+		Name:            fields.metricEventName,
 		Value:           value,
 		Category:        pointy.String(fields.source.String()),
 		Timestamp:       ts,
@@ -163,7 +158,6 @@ func (o *Handler) HandleTrace(w http.ResponseWriter, r *http.Request) {
 				events := span.Events()
 				for l := 0; l < events.Len(); l++ {
 					event := events.At(l)
-					eventAttributes := event.Attributes().AsRaw()
 
 					fields, err := extractFields(ctx, extractFieldsParams{
 						resource: &resource,
@@ -178,8 +172,6 @@ func (o *Handler) HandleTrace(w http.ResponseWriter, r *http.Request) {
 					traceID := cast(fields.requestID, span.TraceID().String())
 					spanID := span.SpanID().String()
 					if event.Name() == semconv.ExceptionEventName {
-						excMessage := cast(eventAttributes[string(semconv.ExceptionMessageKey)], "")
-
 						var logCursor *string
 
 						logRow := clickhouse.NewLogRow(
@@ -187,7 +179,7 @@ func (o *Handler) HandleTrace(w http.ResponseWriter, r *http.Request) {
 							clickhouse.WithTraceID(traceID),
 							clickhouse.WithSpanID(spanID),
 							clickhouse.WithSecureSessionID(fields.sessionID),
-							clickhouse.WithBody(ctx, excMessage),
+							clickhouse.WithBody(ctx, fields.exceptionMessage),
 							clickhouse.WithLogAttributes(fields.attrs),
 							clickhouse.WithServiceName(fields.serviceName),
 							clickhouse.WithServiceVersion(fields.serviceVersion),
@@ -198,7 +190,7 @@ func (o *Handler) HandleTrace(w http.ResponseWriter, r *http.Request) {
 						projectLogs[fields.projectID] = append(projectLogs[fields.projectID], logRow)
 						logCursor = pointy.String(logRow.Cursor())
 
-						isProjectError, backendError := getBackendError(ctx, ts, fields, traceID, spanID, logCursor, excMessage)
+						isProjectError, backendError := getBackendError(ctx, ts, fields, traceID, spanID, logCursor)
 						if backendError == nil {
 							lg(ctx, fields).Error("otel span error got no session and no project")
 						} else {
@@ -209,11 +201,13 @@ func (o *Handler) HandleTrace(w http.ResponseWriter, r *http.Request) {
 							}
 						}
 					} else if event.Name() == highlight.LogEvent {
-						logSev := cast(eventAttributes[string(hlog.LogSeverityKey)], "unknown")
-						logMessage := cast(eventAttributes[string(hlog.LogMessageKey)], "")
-						if logMessage == "" {
+						if fields.logMessage == "" {
 							lg(ctx, fields).Warn("otel received log with no message")
 							continue
+						}
+
+						if fields.logSeverity == "" {
+							fields.logSeverity = "unknown"
 						}
 
 						logRow := clickhouse.NewLogRow(
@@ -221,11 +215,11 @@ func (o *Handler) HandleTrace(w http.ResponseWriter, r *http.Request) {
 							clickhouse.WithTraceID(traceID),
 							clickhouse.WithSpanID(spanID),
 							clickhouse.WithSecureSessionID(fields.sessionID),
-							clickhouse.WithBody(ctx, logMessage),
+							clickhouse.WithBody(ctx, fields.logMessage),
 							clickhouse.WithLogAttributes(fields.attrs),
 							clickhouse.WithServiceName(fields.serviceName),
 							clickhouse.WithServiceVersion(fields.serviceVersion),
-							clickhouse.WithSeverityText(logSev),
+							clickhouse.WithSeverityText(fields.logSeverity),
 							clickhouse.WithSource(fields.source),
 						)
 

backend/otel/extract_test.go
@@ -8,6 +8,8 @@ import (
 	"github.com/highlight/highlight/sdk/highlight-go"
 	"github.com/stretchr/testify/assert"
 	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/ptrace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 )
 
 func newResource(attrs map[string]string) pcommon.Resource {
@@ -31,6 +33,14 @@ func newResource(attrs map[string]string) pcommon.Resource {
 		resource.Attributes().PutStr(k, v)
 	}
 	return resource
+}
+
+func newEvent(attrs map[string]string) ptrace.SpanEvent {
+	event := ptrace.NewSpanEvent()
+	for k, v := range attrs {
+		event.Attributes().PutStr(k, v)
+	}
+	return event
 }
 
 func TestExtractFields_ExtractProjectID(t *testing.T) {
@@ -112,6 +122,92 @@ func TestExtractFields_ExtractRequestID(t *testing.T) {
 	assert.Equal(t, fields.attrs, map[string]string{})
 }
 
+func TestExtractFields_ExtractMetricEventName(t *testing.T) {
+	resource := newResource(map[string]string{
+		highlight.MetricEventName: "metric_name",
+	})
+	fields, err := extractFields(context.TODO(), extractFieldsParams{resource: &resource})
+	assert.NoError(t, err)
+	assert.Equal(t, fields.metricEventName, "metric_name")
+	assert.Equal(t, fields.attrs, map[string]string{})
+}
+
+func TestExtractFields_ExtractMetricEventValue(t *testing.T) {
+	resource := newResource(map[string]string{
+		highlight.MetricEventValue: "99",
+	})
+	fields, err := extractFields(context.TODO(), extractFieldsParams{resource: &resource})
+	assert.NoError(t, err)
+	assert.Equal(t, fields.metricEventValue, "99")
+	assert.Equal(t, fields.attrs, map[string]string{})
+}
+
+func TestExtractFields_ExtractLogSeverity(t *testing.T) {
+	resource := newResource(map[string]string{})
+	event := newEvent(map[string]string{
+		highlight.LogSeverityAttribute: "log_severity",
+	})
+	fields, err := extractFields(context.TODO(), extractFieldsParams{resource: &resource, event: &event})
+	assert.NoError(t, err)
+	assert.Equal(t, fields.logSeverity, "log_severity")
+	assert.Equal(t, fields.attrs, map[string]string{})
+}
+
+func TestExtractFields_ExtractLogMessage(t *testing.T) {
+	resource := newResource(map[string]string{})
+	event := newEvent(map[string]string{
+		highlight.LogMessageAttribute: "log_message",
+	})
+	fields, err := extractFields(context.TODO(), extractFieldsParams{resource: &resource, event: &event})
+	assert.NoError(t, err)
+	assert.Equal(t, fields.logMessage, "log_message")
+	assert.Equal(t, fields.attrs, map[string]string{})
+}
+
+func TestExtractFields_ExtractExceptionType(t *testing.T) {
+	resource := newResource(map[string]string{})
+	event := newEvent(map[string]string{
+		string(semconv.ExceptionTypeKey): "exception_type",
+	})
+	fields, err := extractFields(context.TODO(), extractFieldsParams{resource: &resource, event: &event})
+	assert.NoError(t, err)
+	assert.Equal(t, fields.exceptionType, "exception_type")
+	assert.Equal(t, fields.attrs, map[string]string{})
+}
+
+func TestExtractFields_ExtractExceptionMessage(t *testing.T) {
+	resource := newResource(map[string]string{})
+	event := newEvent(map[string]string{
+		string(semconv.ExceptionMessageKey): "exception_message",
+	})
+	fields, err := extractFields(context.TODO(), extractFieldsParams{resource: &resource, event: &event})
+	assert.NoError(t, err)
+	assert.Equal(t, fields.exceptionMessage, "exception_message")
+	assert.Equal(t, fields.attrs, map[string]string{})
+}
+
+func TestExtractFields_ExtractExceptionStacktrace(t *testing.T) {
+	resource := newResource(map[string]string{})
+	event := newEvent(map[string]string{
+		string(semconv.ExceptionStacktraceKey): "exception_stacktrace",
+	})
+	fields, err := extractFields(context.TODO(), extractFieldsParams{resource: &resource, event: &event})
+	assert.NoError(t, err)
+	assert.Equal(t, fields.exceptionStackTrace, "exception_stacktrace")
+	assert.Equal(t, fields.attrs, map[string]string{})
+}
+
+func TestExtractFields_ExtractErrorURL(t *testing.T) {
+	resource := newResource(map[string]string{})
+	event := newEvent(map[string]string{
+		highlight.ErrorURLAttribute: "error_url",
+	})
+	fields, err := extractFields(context.TODO(), extractFieldsParams{resource: &resource, event: &event})
+	assert.NoError(t, err)
+	assert.Equal(t, fields.errorUrl, "error_url")
+	assert.Equal(t, fields.attrs, map[string]string{})
+}
+
 func TestExtractFields_ExtractServiceName(t *testing.T) {
 	resource := newResource(map[string]string{
 		"service.name": "my_service",
@@ -152,19 +248,6 @@ func TestExtractFields_OmitBackendPropertiesForFrontendSource(t *testing.T) {
 	fields, err := extractFields(context.TODO(), extractFieldsParams{resource: &resource})
 	assert.NoError(t, err)
 	assert.Equal(t, fields.attrs, map[string]string{})
-}
-
-func TestExtractFields_OmitExceptionProperties(t *testing.T) {
-	resource := newResource(map[string]string{
-		"exception.message":    "foo",
-		"exception.stacktrace": "bar",
-		"exception.type":       "baz",
-	})
-	fields, err := extractFields(context.TODO(), extractFieldsParams{resource: &resource})
-	assert.NoError(t, err)
-	assert.Equal(t, fields.attrs, map[string]string{
-		"exception.type": "baz",
-	})
 }
 
 func TestExtractFields_TrimLongFields(t *testing.T) {

sdk/highlight-go/otel.go
@@ -3,6 +3,10 @@ package highlight
 import (
 	"context"
 	"fmt"
+	"net/url"
+	"reflect"
+	"strings"
+
 	"github.com/pkg/errors"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
@@ -12,9 +16,6 @@ import (
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 	"go.opentelemetry.io/otel/trace"
-	"net/url"
-	"reflect"
-	"strings"
 )
 
 const OTLPDefaultEndpoint = "https://otel.highlight.io:4318"
@@ -37,23 +38,6 @@ const LogMessageAttribute = "log.message"
 const MetricEvent = "metric"
 const MetricEventName = "metric.name"
 const MetricEventValue = "metric.value"
-
-var InternalAttributePrefixes = []string{
-	DeprecatedProjectIDAttribute,
-	DeprecatedSessionIDAttribute,
-	DeprecatedRequestIDAttribute,
-	DeprecatedSourceAttribute,
-	ProjectIDAttribute,
-	SessionIDAttribute,
-	RequestIDAttribute,
-	SourceAttribute,
-	LogMessageAttribute,
-	LogSeverityAttribute,
-	// exception should be parsed as structured and not included as part of log attributes
-	"exception.message",
-	"exception.stacktrace",
-	"fluent.",
-}
 
 var BackendOnlyAttributePrefixes = []string{
 	"container.",

commit 125c665aaea8f68d1748fb7c6dd3b77b1e9f8560

fix panic when extracting metric value (#6137)
## Summary

<!--
Ideally, there is an attached GitHub issue that will describe the "why".

If relevant, use this section to call out any additional information
you'd like to _highlight_ to the reviewer.
-->

A regression was introduced in #6121 such that metric values were being
casted to a string and causing a panic.

![Screenshot 2023-07-27 at 10 22 26
AM](https://github.com/highlight/highlight/assets/58678/bb4be26a-4544-4ba9-8ecd-81884c9ab459)

Long term, we probably need to consider whether this `.(type)` syntax is
actually safe to be using. It would be better to try and use builtin
functions that return errors when we can't safely parse a value.


## How did you test this change?

<!--
Frontend - Leave a screencast or a screenshot to visually describe the
changes.
-->

Confirmed that the panic does not occur when a new metric comes in.

## Are there any deployment considerations?

<!--
 Backend - Do we need to consider migrations or backfilling data?
-->

N/A

backend/otel/extract.go
@@ -38,7 +38,7 @@ type extractedFields struct {
 	errorUrl            string
 
 	metricEventName  string
-	metricEventValue string // up to the consumer to parse this into an expected format
+	metricEventValue float64
 
 	// This represents the merged result of resource, span...log attributes
 	// _after_ we extract fields out. In other words, if `serviceName` is extracted, it won't be included
@@ -144,8 +144,20 @@ func extractFields(ctx context.Context, params extractFieldsParams) (extractedFi
 	}
 
 	if val, ok := attrs[highlight.MetricEventValue]; ok {
-		fields.metricEventValue = val.(string)
-		delete(attrs, highlight.MetricEventValue)
+		float64Value, ok := val.(float64)
+		if ok {
+			fields.metricEventValue = float64Value
+			delete(attrs, highlight.MetricEventValue)
+		} else {
+			stringValue, ok := val.(string)
+			if ok {
+				parsedFloat64Value, err := strconv.ParseFloat(stringValue, 64)
+				if err == nil {
+					fields.metricEventValue = parsedFloat64Value
+					delete(attrs, highlight.MetricEventValue)
+				}
+			}
+		}
 	}
 
 	if val, ok := eventAttributes[string(semconv.ExceptionTypeKey)]; ok { // we know that exception.type will be in the event attributes map

backend/otel/otel.go
@@ -7,7 +7,6 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
-	"strconv"
 	"time"
 
 	"github.com/samber/lo"
@@ -87,16 +86,11 @@ func getMetric(ctx context.Context, ts time.Time, fields extractedFields, traceI
 	if fields.metricEventName == "" {
 		return nil, e.New("otel received metric with no name")
 	}
-	value, err := strconv.ParseFloat(fields.metricEventValue, 64)
-
-	if err != nil {
-		return nil, e.New("otel received metric with no value")
-	}
 	return &model.MetricInput{
 		SessionSecureID: fields.sessionID,
 		Group:           pointy.String(fields.requestID),
 		Name:            fields.metricEventName,
-		Value:           value,
+		Value:           fields.metricEventValue,
 		Category:        pointy.String(fields.source.String()),
 		Timestamp:       ts,
 		Tags: lo.Map(lo.Entries(fields.attrs), func(t lo.Entry[string, string], i int) *model.MetricTag {

backend/otel/extract_test.go
@@ -12,7 +12,7 @@ import (
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 )
 
-func newResource(attrs map[string]string) pcommon.Resource {
+func newResource(t *testing.T, attrs map[string]any) pcommon.Resource {
 	resource := pcommon.NewResource()
 
 	containsProjectID := false
@@ -30,7 +30,17 @@ func newResource(attrs map[string]string) pcommon.Resource {
 	}
 
 	for k, v := range attrs {
-		resource.Attributes().PutStr(k, v)
+		c, ok := v.(string)
+		if ok {
+			resource.Attributes().PutStr(k, c)
+		} else {
+			c, ok := v.(float64)
+			if ok {
+				resource.Attributes().PutDouble(k, c)
+			} else {
+				assert.Fail(t, "Failed to set resource value.")
+			}
+		}
 	}
 	return resource
 }
@@ -44,7 +54,7 @@ func newEvent(attrs map[string]string) ptrace.SpanEvent {
 }
 
 func TestExtractFields_ExtractProjectID(t *testing.T) {
-	resource := newResource(map[string]string{
+	resource := newResource(t, map[string]any{
 		highlight.DeprecatedProjectIDAttribute: "1",
 	})
 	fields, err := extractFields(context.TODO(), extractFieldsParams{resource: &resource})
@@ -52,7 +62,7 @@ func TestExtractFields_ExtractProjectID(t *testing.T) {
 	assert.Equal(t, fields.projectID, "1")
 	assert.Equal(t, fields.attrs, map[string]string{})
 
-	resource = newResource(map[string]string{
+	resource = newResource(t, map[string]any{
 		highlight.ProjectIDAttribute: "1",
 	})
 	fields, err = extractFields(context.TODO(), extractFieldsParams{resource: &resource})
@@ -69,7 +79,7 @@ func TestExtractFields_ExtractProjectID(t *testing.T) {
 }
 
 func TestExtractFields_ExtractSessionID(t *testing.T) {
-	resource := newResource(map[string]string{
+	resource := newResource(t, map[string]any{
 		highlight.DeprecatedSessionIDAttribute: "session_abc",
 	})
 	fields, err := extractFields(context.TODO(), extractFieldsParams{
@@ -79,7 +89,7 @@ func TestExtractFields_ExtractSessionID(t *testing.T) {
 	assert.Equal(t, fields.sessionID, "session_abc")
 	assert.Equal(t, fields.attrs, map[string]string{})
 
-	resource = newResource(map[string]string{
+	resource = newResource(t, map[string]any{
 		highlight.SessionIDAttribute: "session_abc",
 	})
 	fields, err = extractFields(context.TODO(), extractFieldsParams{resource: &resource})
@@ -89,13 +99,13 @@ func TestExtractFields_ExtractSessionID(t *testing.T) {
 }
 
 func TestExtractFields_ExtractSource(t *testing.T) {
-	resource := newResource(map[string]string{})
+	resource := newResource(t, map[string]any{})
 	fields, err := extractFields(context.TODO(), extractFieldsParams{resource: &resource})
 	assert.NoError(t, err)
 	assert.Equal(t, fields.source, modelInputs.LogSourceBackend) // defaults to backend
 	assert.Equal(t, fields.attrs, map[string]string{})
 
-	resource = newResource(map[string]string{
+	resource = newResource(t, map[string]any{
 		highlight.DeprecatedSourceAttribute: modelInputs.LogSourceFrontend.String(),
 	})
 	fields, err = extractFields(context.TODO(), extractFieldsParams{resource: &resource})
@@ -103,7 +113,7 @@ func TestExtractFields_ExtractSource(t *testing.T) {
 	assert.Equal(t, fields.source, modelInputs.LogSourceFrontend)
 	assert.Equal(t, fields.attrs, map[string]string{})
 
-	resource = newResource(map[string]string{
+	resource = newResource(t, map[string]any{
 		highlight.SourceAttribute: modelInputs.LogSourceFrontend.String(),
 	})
 	fields, err = extractFields(context.TODO(), extractFieldsParams{resource: &resource})
@@ -113,7 +123,7 @@ func TestExtractFields_ExtractSource(t *testing.T) {
 }
 
 func TestExtractFields_ExtractRequestID(t *testing.T) {
-	resource := newResource(map[string]string{
+	resource := newResource(t, map[string]any{
 		highlight.RequestIDAttribute: "request_id",
 	})
 	fields, err := extractFields(context.TODO(), extractFieldsParams{resource: &resource})
@@ -123,7 +133,7 @@ func TestExtractFields_ExtractRequestID(t *testing.T) {
 }
 
 func TestExtractFields_ExtractMetricEventName(t *testing.T) {
-	resource := newResource(map[string]string{
+	resource := newResource(t, map[string]any{
 		highlight.MetricEventName: "metric_name",
 	})
 	fields, err := extractFields(context.TODO(), extractFieldsParams{resource: &resource})
@@ -133,17 +143,27 @@ func TestExtractFields_ExtractMetricEventName(t *testing.T) {
 }
 
 func TestExtractFields_ExtractMetricEventValue(t *testing.T) {
-	resource := newResource(map[string]string{
+	resource := newResource(t, map[string]any{
+		highlight.MetricEventValue: float64(99),
+	})
+	fields, err := extractFields(context.TODO(), extractFieldsParams{resource: &resource})
+	assert.NoError(t, err)
+	assert.Equal(t, fields.metricEventValue, float64(99))
+	assert.Equal(t, fields.attrs, map[string]string{})
+}
+
+func TestExtractFields_ExtractMetricEventValueHandlesBadInput(t *testing.T) {
+	resource := newResource(t, map[string]any{
 		highlight.MetricEventValue: "99",
 	})
 	fields, err := extractFields(context.TODO(), extractFieldsParams{resource: &resource})
 	assert.NoError(t, err)
-	assert.Equal(t, fields.metricEventValue, "99")
+	assert.Equal(t, fields.metricEventValue, float64(99))
 	assert.Equal(t, fields.attrs, map[string]string{})
 }
 
 func TestExtractFields_ExtractLogSeverity(t *testing.T) {
-	resource := newResource(map[string]string{})
+	resource := newResource(t, map[string]any{})
 	event := newEvent(map[string]string{
 		highlight.LogSeverityAttribute: "log_severity",
 	})
@@ -154,7 +174,7 @@ func TestExtractFields_ExtractLogSeverity(t *testing.T) {
 }
 
 func TestExtractFields_ExtractLogMessage(t *testing.T) {
-	resource := newResource(map[string]string{})
+	resource := newResource(t, map[string]any{})
 	event := newEvent(map[string]string{
 		highlight.LogMessageAttribute: "log_message",
 	})
@@ -165,7 +185,7 @@ func TestExtractFields_ExtractLogMessage(t *testing.T) {
 }
 
 func TestExtractFields_ExtractExceptionType(t *testing.T) {
-	resource := newResource(map[string]string{})
+	resource := newResource(t, map[string]any{})
 	event := newEvent(map[string]string{
 		string(semconv.ExceptionTypeKey): "exception_type",
 	})
@@ -176,7 +196,7 @@ func TestExtractFields_ExtractExceptionType(t *testing.T) {
 }
 
 func TestExtractFields_ExtractExceptionMessage(t *testing.T) {
-	resource := newResource(map[string]string{})
+	resource := newResource(t, map[string]any{})
 	event := newEvent(map[string]string{
 		string(semconv.ExceptionMessageKey): "exception_message",
 	})
@@ -187,7 +207,7 @@ func TestExtractFields_ExtractExceptionMessage(t *testing.T) {
 }
 
 func TestExtractFields_ExtractExceptionStacktrace(t *testing.T) {
-	resource := newResource(map[string]string{})
+	resource := newResource(t, map[string]any{})
 	event := newEvent(map[string]string{
 		string(semconv.ExceptionStacktraceKey): "exception_stacktrace",
 	})
@@ -198,7 +218,7 @@ func TestExtractFields_ExtractExceptionStacktrace(t *testing.T) {
 }
 
 func TestExtractFields_ExtractErrorURL(t *testing.T) {
-	resource := newResource(map[string]string{})
+	resource := newResource(t, map[string]any{})
 	event := newEvent(map[string]string{
 		highlight.ErrorURLAttribute: "error_url",
 	})
@@ -209,7 +229,7 @@ func TestExtractFields_ExtractErrorURL(t *testing.T) {
 }
 
 func TestExtractFields_ExtractServiceName(t *testing.T) {
-	resource := newResource(map[string]string{
+	resource := newResource(t, map[string]any{
 		"service.name": "my_service",
 	})
 	fields, err := extractFields(context.TODO(), extractFieldsParams{resource: &resource})
@@ -219,7 +239,7 @@ func TestExtractFields_ExtractServiceName(t *testing.T) {
 }
 
 func TestExtractFields_ExtractServiceVersion(t *testing.T) {
-	resource := newResource(map[string]string{
+	resource := newResource(t, map[string]any{
 		"service.version": "abc123",
 	})
 	fields, err := extractFields(context.TODO(), extractFieldsParams{resource: &resource})
@@ -229,7 +249,7 @@ func TestExtractFields_ExtractServiceVersion(t *testing.T) {
 }
 
 func TestExtractFields_OmitLogSeverity(t *testing.T) {
-	resource := newResource(map[string]string{
+	resource := newResource(t, map[string]any{
 		"os.description": "Debian GNU/Linux 11 (bullseye)",
 		"log.severity":   "info", // should be skipped since this is an internal attribute
 	})
@@ -241,7 +261,7 @@ func TestExtractFields_OmitLogSeverity(t *testing.T) {
 }
 
 func TestExtractFields_OmitBackendPropertiesForFrontendSource(t *testing.T) {
-	resource := newResource(map[string]string{
+	resource := newResource(t, map[string]any{
 		"os.description":   "Debian GNU/Linux 11 (bullseye)", // should be skipped since this is a frontend source
 		"highlight.source": "frontend",
 	})
@@ -256,7 +276,7 @@ func TestExtractFields_TrimLongFields(t *testing.T) {
 		value += "a"
 	}
 
-	resource := newResource(map[string]string{
+	resource := newResource(t, map[string]any{
 		"foo": value,
 	})
 	fields, err := extractFields(context.TODO(), extractFieldsParams{resource: &resource})